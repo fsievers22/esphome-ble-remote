1ffe35edb030830d524621048a70881148d4118e
Fabian Sievers <fabian.sievers@outlook.com>
2023-01-15 12:44:25 +0100

    Separate component and text sensor

--- components/ble_client_hid/__init__.py
+++ components/ble_client_hid/__init__.py
@@ -1,1 +1,43 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import ble_client
+from esphome.const import CONF_ID
+
+
+DEPENDENCIES = ['ble_client']
 CODE_OWNERS=["@fsievers22"]
+
+ble_client_hid_ns = cg.esphome_ns.namespace("ble_client_hid")
+
+BLEClientHID = ble_client_hid_ns.class_(
+    "BLEClientHID",
+    cg.PollingComponent,
+    ble_client.BLEClientNode,
+)
+
+CONFIG_SCHEMA = (
+    cv.Schema(
+        {
+            cv.GenerateID(): cv.declare_id(BLEClientHID)
+        }
+    )
+    .extend(cv.polling_component_schema("60s"))
+    .extend(ble_client.BLE_CLIENT_SCHEMA)
+)
+
+CONF_BLE_CLIENT_HID_ID = "ble_client_hid_id"
+
+BLE_CLIENT_HID_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(CONF_BLE_CLIENT_HID_ID): cv.use_id(BLEClientHID),
+    }
+)
+
+async def register_keycode_text_sensor(var, config):
+    parent = await cg.get_variable(config[CONF_BLE_CLIENT_HID_ID])
+    cg.add(parent.register_keycode_text_sensor(var))
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    await ble_client.register_ble_node(var, config)
--- components/ble_client_hid/ble_client_hid.h
+++ components/ble_client_hid/ble_client_hid.h
@@ -9,13 +9,12 @@
 #include <esp_gattc_api.h>
 
 namespace esphome {
-namespace ble_client {
+namespace ble_client_hid {
 
 namespace espbt = esphome::esp32_ble_tracker;
 
-class BLEHIDKeyTextSensor : public text_sensor::TextSensor, public PollingComponent, public BLEClientNode {
+class BLEClientHID : public PollingComponent, public ble_client::BLEClientNode {
  public:
-  void setup() override;
   void loop() override;
   void update() override;
   void gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t gattc_if,
@@ -23,15 +22,16 @@ class BLEHIDKeyTextSensor : public text_sensor::TextSensor, public PollingCompon
 
   void dump_config() override;
   float get_setup_priority() const override { return setup_priority::AFTER_BLUETOOTH; }
-  void ble_security_setup();
+  void register_keycode_text_sensor(text_sensor::TextSensor *keycode_text_sensor);
   std::string parse_data(uint8_t *value, uint16_t value_len);
 
  protected:
-  std::vector<BLECharacteristic *> characteristics;
+  std::vector<ble_client::BLECharacteristic *> characteristics;
   std::vector<uint16_t> handles_registered_for_notify;
-  espbt::ESPBTUUID descr_uuid_;
+  text_sensor::TextSensor *keycode_text_sensor;
+
 };
 
-}  // namespace ble_client
+}  // namespace ble_client_hid
 }  // namespace esphome
 #endif
