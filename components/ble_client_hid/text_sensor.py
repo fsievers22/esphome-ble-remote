ebc8763db6e13557c97a0b810a537a960469f887
benedikt-bartscher <31854409+benedikt-bartscher@users.noreply.github.com>
2023-04-30 23:17:02 +0200

    fix text_sensor

--- components/ble_client_hid/text_sensor.py
+++ components/ble_client_hid/text_sensor.py
@@ -1,7 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import text_sensor
-from esphome.const import CONF_ID
 from esphome.components import ble_client_hid
 
 
@@ -21,5 +20,5 @@ CONFIG_SCHEMA = cv.All(
 )
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
+    var = await text_sensor.new_text_sensor(config)
     await ble_client_hid.register_last_event_usage_text_sensor(var, config)
